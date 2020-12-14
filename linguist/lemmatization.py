e268016f158323fc5d187e6ec106c9bb97e233dd
Oleksandr Kosovan <sanya.kosovan@gmail.com>
2020-12-14 15:08:07 +0200

    Lemmatization module

--- linguist/lemmatization.py
+++ linguist/lemmatization.py
@@ -1,1 +1,26 @@
-from pymo
+import pymorphy2
+
+class UkLemmatizer():
+
+    def __init__(self):
+        self.morph = pymorphy2.MorphAnalyzer(lang='uk')
+    
+    def token_lemmatization(self, token):
+        """
+        input: token (one word)
+        output: lemmitized token (simple word form)
+        """
+        return self.morph.parse(token)[0].normal_form
+
+    
+    def tokens_lemmatization(self, tokens):
+        """
+        input: tokens list of sentence/text without punctuation
+        output: lemmitized tokens list 
+        """
+        lemmitized_tokens = []
+        for word in tokens:
+            result = self.morph.parse(word)[0].normal_form
+            lemmitized_tokens.append(result)
+        return lemmitized_tokens
+
--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-
 print("hello Ukrainization!!!")
+
+
+from linguist.lemmatization import UkLemmatizer
+
+lemm = UkLemmatizer()
+
+token = 'чорніє'
+
+
+result = lemm.token_lemmatization(token=token)
+print(result)
+
+sentence = 'Дорогу сестру збираю у дорогу а брати вирішили не брати машину'
+
+result = lemm.tokens_lemmatization(tokens=sentence.split(" "))
+
+print(result)
 
