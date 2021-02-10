b6513eee7bd48c3e07499a3f9b77b3eb0e4fc545
Oleksandr Kosovan <sanya.kosovan@gmail.com>
2021-02-10 16:16:27 +0200

    Updated stemming doc

--- normalization/stemming.py
+++ normalization/stemming.py
@@ -5,18 +5,22 @@ __version__ = 1.0
 
 class UkStemmer:
     def __init__(self):
-        # http://uk.wikipedia.org/wiki/Голосний_звук
+        """
+        Useful links:
+         - vowel: uk.wikipedia.org/wiki/Голосний_звук
+         - perfectiveground: uk.wikipedia.org/wiki/Рефлексивне_дієслово
+         - adjective: uk.wikipedia.org/wiki/Прикметник + wapedia.mobi/uk/Прикметник
+         - participle: uk.wikipedia.org/wiki/Дієприкметник
+         - verb: http://uk.wikipedia.org/wiki/Дієслово
+         - noun: uk.wikipedia.org/wiki/Іменник
+         - Code: https://github.com/Desklop/Uk_Stemmer/blob/master/uk_stemmer/uk_stemmer.py
+        """
         self.vowel = r'аеиоуюяіїє'
         self.perfectiveground = r'(ив|ивши|ившись|ыв|ывши|ывшись((?<=[ая])(в|вши|вшись)))$'
-        # http://uk.wikipedia.org/wiki/Рефлексивне_дієслово
         self.reflexive = r'(с[яьи])$'
-        # http://uk.wikipedia.org/wiki/Прикметник + http://wapedia.mobi/uk/Прикметник
         self.adjective = r'(ими|ій|ий|а|е|ова|ове|ів|є|їй|єє|еє|я|ім|ем|им|ім|их|іх|ою|йми|іми|у|ю|ого|ому|ої)$'
-        # http://uk.wikipedia.org/wiki/Дієприкметник
         self.participle = r'(ий|ого|ому|им|ім|а|ій|у|ою|ій|і|их|йми|их)$'
-        # http://uk.wikipedia.org/wiki/Дієслово
         self.verb = r'(сь|ся|ив|ать|ять|у|ю|ав|али|учи|ячи|вши|ши|е|ме|ати|яти|є)$'
-        # http://uk.wikipedia.org/wiki/Іменник
         self.noun = r'(а|ев|ов|е|ями|ами|еи|и|ей|ой|ий|й|иям|ям|ием|ем|ам|ом|о|у|ах|иях|ях|ы|ь|ию|ью|ю|ия|ья|я|і|ові|ї|ею|єю|ою|є|еві|ем|єм|ів|їв|ю)$'
         self.rvre = r'[аеиоуюяіїє]'
         self.derivational = r'[^аеиоуюяіїє][аеиоуюяіїє]+[^аеиоуюяіїє]+[аеиоуюяіїє].*(?<=о)сть?$'
@@ -79,5 +83,3 @@ class UkStemmer:
         This method is used to simulate the PyStemmer interface (https://github.com/snowballstem/pystemmer). '''
 
         return self.stem_word(word)
-
-# Code: https://github.com/Desklop/Uk_Stemmer/blob/master/uk_stemmer/uk_stemmer.py
